4a9b9410dec10690b9b05cd363d9b04adce029cb
Chavalentas <stefanchvala@gmail.com>
2022-10-28 17:42:45 +0200

    Add simpler app

---
+++ nodeapp/test/test.js
@@ -0,0 +1,23 @@
+var assert = require('assert');
+var server = require('../index');
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+
+chai.should();
+chai.use(chaiHttp);
+
+describe('BackendTest', function(){
+    describe('Test the GET', () => {
+        it('should return Hello world', (done) => {
+            chai.request(server)
+            .get('/')
+            .end((err, response) => {
+                response.should.have.status(200);
+                response.body.should.be.a('object');
+                response.body.should.have.property('message');
+                response.body.should.have.property('message').eql('Hello World');
+                done();
+            })
+        });
+    });
+});
